635a455e66977e0d9524e49d3b74a4b45e043122
Selma-mtch <mataiche.selma77@gmail.com>
2025-12-20 00:49:57 +0100

    correction ajout pannier

--- web/templates/static/js/main.js
+++ web/templates/static/js/main.js
@@ -5,3 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // HTMX: le token CSRF est injecté côté base.html; pas d'action ici.
+
+// Petite notification après ajout panier (évite les erreurs si la fonction est appelée côté templates)
+function showAddedToast() {
+  // Rien de complexe ici, on laisse le flash backend gérer l'info.
+}
